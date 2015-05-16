901cba228377807a7407c96ba68c6f06315d8454
Martin Engström <martin.engstrom@gmail.com>
2015-05-16 23:21:33 +0200

    minor update

--- airplay_config.sh
+++ airplay_config.sh
@@ -1,4 +1,33 @@
- sudo useradd -g audio shairport
- sudo cp scripts/debian/init.d/shairport /etc/init.d/
- sudo chmod +x /etc/init.d/shairport
- sudo update-rc.d shairport defaults
+#!/bin/bash
+
+read -p "Airplay device name: " AP_NAME
+
+#--------------------------------------------------------------------
+function tst {
+    echo "Executing: $*"
+    if ! $*; then
+        echo "Exiting scrip due to error from: $*"
+        exit 1
+    fi
+}
+#--------------------------------------------------------------------
+
+# Configure shairplay
+
+tst cd ~/projects/airplay-audio-project/shairport
+tst sudo cp scripts/debian/init.d/shairport /etc/init.d/
+tst sudo chmod +x /etc/init.d/shairport
+tst sudo update-rc.d shairport defaults
+sudo patch /etc/init.d/shairport << EOT
+***************
+*** 20,22 ****
+  DESC="Shairport Airtunes emulator"
+! NAME=shairport
+  DAEMON=/usr/local/bin/shairport
+--- 20,23 ----
+  DESC="Shairport Airtunes emulator"
+! #NAME=shairport
+! NAME="${AP_NAME}"
+  DAEMON=/usr/local/bin/shairport
+EOT
+tst sudo useradd -g audio shairport
--- airplay_install.sh
+++ airplay_install.sh
@@ -1,17 +1,31 @@
- cd ~
- sudo apt-get -y install  libao-dev libssl-dev git avahi-utils libwww-perl
- mkdir projects
- cd projects
- mkdir airplay-audio-project
- cd airplay-audio-project
- git clone https://github.com/njh/perl-net-sdp.git
- cd perl-net-sdp
- perl Build.PL
- ./Build
- ./Build test
- sudo ./Build install
- cd ..
- git clone https://github.com/abrasive/shairport.git
- cd shairport
- make
- sudo make install
+#!/bin/bash
+
+#--------------------------------------------------------------------
+function tst {
+    echo "Executing: $*"
+    if ! $*; then
+        echo "Exiting scrip due to error from: $*"
+        exit 1
+    fi
+}
+#--------------------------------------------------------------------
+
+# Install shairplay from source
+
+tst cd ~
+tst sudo apt-get -y install  libao-dev libssl-dev git avahi-utils libwww-perl
+tst mkdir projects
+tst cd projects
+tst mkdir airplay-audio-project
+tst cd airplay-audio-project
+tst git clone https://github.com/njh/perl-net-sdp.git
+tst cd perl-net-sdp
+tst perl Build.PL
+tst ./Build
+tst ./Build test
+tst sudo ./Build install
+tst cd ..
+tst git clone https://github.com/abrasive/shairport.git
+tst cd shairport
+tst make
+tst sudo make install
