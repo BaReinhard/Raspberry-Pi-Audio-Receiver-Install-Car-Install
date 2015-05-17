fd753d406f33b1c7e52eab34c284a1fbcbb9d204
Martin Engström <martin.engstrom@gmail.com>
2015-05-17 22:44:04 +0200

    diff fixed

--- airplay_config.sh
+++ airplay_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -p "Airplay device name: " AP_NAME
+read -p "Airplay device name: " MYNAME
 
 #--------------------------------------------------------------------
 function tst {
@@ -20,14 +20,14 @@ tst sudo chmod +x /etc/init.d/shairport
 tst sudo update-rc.d shairport defaults
 sudo patch /etc/init.d/shairport << EOT
 ***************
-*** 20,22 ****
-  DESC="Shairport Airtunes emulator"
-! NAME=shairport
-  DAEMON=/usr/local/bin/shairport
---- 20,23 ----
-  DESC="Shairport Airtunes emulator"
-! #NAME=shairport
-! NAME="${AP_NAME}"
-  DAEMON=/usr/local/bin/shairport
+*** 29,31 ****
+  PIDFILE=/var/run/$NAME.pid
+! AP_NAME=\$(hostname)
+  NICE=0
+--- 29,32 ----
+  PIDFILE=/var/run/$NAME.pid
+! #AP_NAME=\$(hostname)
+! AP_NAME=${MYNAME}
+  NICE=0
 EOT
 tst sudo useradd -g audio shairport
