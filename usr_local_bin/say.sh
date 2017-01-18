cda3e559cc19240e4f129e74b7a69616f55dd4f0
Aden Beckitt <adenbeckitt@gmail.com>
2017-01-18 03:15:28 +0000

    Fix google TTS url

--- usr_local_bin/say.sh
+++ usr_local_bin/say.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 say() {
     local IFS=+;
-    if ! /usr/bin/mplayer -softvol -volume 60 -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?tl=en&q=$*"; then
+    if ! /usr/bin/mplayer -softvol -volume 60 -ao alsa -really-quiet -noconsolecontrols "https://translate.google.com/translate_tts?ie=UTF-8&tl=en&client=tw-ob&q=$*"; then
         espeak $*
     fi
 } 2>/dev/null
 
 say $*
-
