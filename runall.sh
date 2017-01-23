47f4cc875288a89d31e7074b34473052c3ff3438
Aden Beckitt <adenbeckitt@gmail.com>
2017-01-23 20:22:33 +0000

    Added prep file

--- runall.sh
+++ runall.sh
@@ -9,13 +9,15 @@ read -p "Device name: " MYNAME
 function tst {
     echo "===> Executing: $*"
     if ! $*; then
-        echo "Exiting scrip due to error from: $*"
+        echo "Exiting script due to error from: $*"
         exit 1
     fi
 }
 #--------------------------------------------------------------------
 
 echo "Starting @ `date`" | tee -a $log
+echo "--------------------------------------------" | tee -a $log
+tst ./bt_pa_prep.sh | tee -a $log
 echo "--------------------------------------------" | tee -a $log
 tst ./bt_pa_install.sh | tee -a $log
 echo "--------------------------------------------" | tee -a $log
