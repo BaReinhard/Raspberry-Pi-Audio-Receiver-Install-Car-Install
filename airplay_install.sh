469a4813ab1b0b543a37c9b3cb080a3443f70c2b
Aden Beckitt <adenbeckitt@gmail.com>
2017-01-23 20:19:08 +0000

    Complete change. Now installs shairport-sync

--- airplay_install.sh
+++ airplay_install.sh
@@ -4,28 +4,25 @@
 function tst {
     echo "===> Executing: $*"
     if ! $*; then
-        echo "Exiting scrip due to error from: $*"
+        echo "Exiting script due to error from: $*"
         exit 1
     fi
 }
 #--------------------------------------------------------------------
 
-# Install shairplay from source
+# Install shairplay-sync from source
 
-tst cd ~
-tst sudo apt-get -y install  libao-dev libssl-dev git avahi-utils libwww-perl
-mkdir projects
-tst cd projects
-mkdir airplay-audio-project
-tst cd airplay-audio-project
-tst git clone https://github.com/njh/perl-net-sdp.git
-tst cd perl-net-sdp
-tst perl Build.PL
-tst ./Build
-tst ./Build test
-tst sudo ./Build install
-tst cd ..
-tst git clone https://github.com/abrasive/shairport.git
-tst cd shairport
+tst sudo apt-get -y install build-essential git autoconf automake libtool libdaemon-dev libasound2-dev libpopt-dev libconfig-dev avahi-daemon libavahi-client-dev libssl-dev
+mkdir airplay
+tst cd airplay
+tst git clone https://github.com/mikebrady/shairport-sync.git
+tst cd shairport-sync
+tst autoreconf -i -f
+tst ./configure --with-alsa --with-avahi --with-ssl=openssl --with-metadata --with-systemd --sysconfdir=/etc
 tst make
+tst getent group shairport-sync &>/dev/null || sudo groupadd -r shairport-sync >/dev/null
+tst getent passwd shairport-sync &> /dev/null || sudo useradd -r -M -g shairport-sync -s /usr/bin/nologin -G audio shairport-sync >/dev/null
 tst sudo make install
+tst sudo systemctl enable shairport-sync
+
+echo "Done! You should reboot now"
