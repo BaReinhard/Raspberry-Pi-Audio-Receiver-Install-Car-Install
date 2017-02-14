3064f7cf49f0f545c7cbb14a18b4d5440ac243d7
Brett Reinhard <brett@reinhards.us>
2017-02-14 16:16:25 +0000

    airplay_install.sh edited online with Bitbucket

--- airplay_install.sh
+++ airplay_install.sh
@@ -16,11 +16,11 @@ tst sudo apt-get -y install build-essential git autoconf automake libtool libdae
 tst cd /~
 tst git clone https://github.com/mikebrady/shairport-sync.git
 tst cd shairport-sync
-tst autoreconf -i -f
-tst ./configure --sysconfdir=/etc --with-alsa --with-avahi --with-ssl=openssl --with-metadata --with-soxr --with-systemd
-tst make
-tst getent group shairport-sync &>/dev/null || sudo groupadd -r shairport-sync >/dev/null
-tst getent passwd shairport-sync &> /dev/null || sudo useradd -r -M -g shairport-sync -s /usr/bin/nologin -G audio shairport-sync >/dev/null
+tst sudo autoreconf -i -f
+tst sudo ./configure --sysconfdir=/etc --with-alsa --with-avahi --with-ssl=openssl --with-metadata --with-soxr --with-systemd
+tst sudo make
+tst sudo getent group shairport-sync &>/dev/null || sudo groupadd -r shairport-sync >/dev/null
+tst sudo getent passwd shairport-sync &> /dev/null || sudo useradd -r -M -g shairport-sync -s /usr/bin/nologin -G audio shairport-sync >/dev/null
 tst sudo make install
 tst sudo systemctl enable shairport-sync
 
