fcc03c16ff8e5f4adaa1b0375e4b9aee376328a8
Brett Reinhard <brett@reinhards.us>
2017-02-14 15:11:00 +0000

    airplay_install.sh edited online with Bitbucket

--- airplay_install.sh
+++ airplay_install.sh
@@ -12,13 +12,13 @@ function tst {
 
 # Install shairplay-sync from source
 
-tst sudo apt-get -y install build-essential git autoconf automake libtool libdaemon-dev libasound2-dev libpopt-dev libconfig-dev avahi-daemon libavahi-client-dev libssl-dev
-mkdir airplay
-tst cd airplay
+tst sudo apt-get -y install build-essential git autoconf automake libtool libdaemon-dev libasound2-dev libpopt-dev libconfig-dev avahi-daemon libavahi-client-dev libssl-dev libpolarssl-dev libsoxr-dev
+mkdir ~/
+tst cd /~
 tst git clone https://github.com/mikebrady/shairport-sync.git
 tst cd shairport-sync
 tst autoreconf -i -f
-tst ./configure --with-alsa --with-avahi --with-ssl=openssl --with-metadata --with-systemd --sysconfdir=/etc
+tst ./configure --sysconfdir=/etc --with-alsa --with-avahi --with-ssl=openssl --with-metadata --with-soxr --with-systemd
 tst make
 tst getent group shairport-sync &>/dev/null || sudo groupadd -r shairport-sync >/dev/null
 tst getent passwd shairport-sync &> /dev/null || sudo useradd -r -M -g shairport-sync -s /usr/bin/nologin -G audio shairport-sync >/dev/null
