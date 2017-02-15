71d7f37204bb16d0b7c6b04f5dbd96f6cbccb051
Brett Reinhard <brett@reinhards.us>
2017-02-15 07:01:30 -0800

    Update sound_card_install.sh

--- sound_card_install.sh
+++ sound_card_install.sh
@@ -2,9 +2,8 @@
 # This file will add the proper sound card configurations to your /etc/asound.conf
 # I am unsure whether or not you need pulse and pulse_connect, but will be testing if its absolutely necessary.
 # If you are not using any sound card please change all the following line of "card 1" to "card 0"
-# If you are using a hifiberry or any other dac, you will need to replace "snd-usb-audio" to the name of the driver you are using
 sudo cat <<EOT >> /etc/modprobe.d/alsa-base.conf
-options snd-usb-audio index=0
+options snd_bcm2835 index=0
 EOT
 sudo cat <<EOT >> /etc/asound.conf
 pcm.pulse {
