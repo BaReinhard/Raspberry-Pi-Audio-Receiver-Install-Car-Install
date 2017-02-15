7c7d76fe507f114483a158e1c7d0d92b2f29ec8c
Brett Reinhard <brett@reinhards.us>
2017-02-14 19:25:00 -0800

    Update airplay_install.sh

--- airplay_install.sh
+++ airplay_install.sh
@@ -13,15 +13,19 @@ function tst {
 # Install shairplay-sync from source
 
 tst sudo apt-get -y install build-essential git autoconf automake libtool libdaemon-dev libasound2-dev libpopt-dev libconfig-dev avahi-daemon libavahi-client-dev libssl-dev libpolarssl-dev libsoxr-dev
-tst cd /~
+tst cd /$HOME
 tst git clone https://github.com/mikebrady/shairport-sync.git
 tst cd shairport-sync
-tst sudo autoreconf -i -f
-tst sudo ./configure --sysconfdir=/etc --with-alsa --with-avahi --with-ssl=openssl --with-metadata --with-soxr --with-systemd
-tst sudo make
-tst sudo getent group shairport-sync &>/dev/null || sudo groupadd -r shairport-sync >/dev/null
-tst sudo getent passwd shairport-sync &> /dev/null || sudo useradd -r -M -g shairport-sync -s /usr/bin/nologin -G audio shairport-sync >/dev/null
+tst autoreconf -i -f
+tst ./configure --sysconfdir=/etc --with-alsa --with-avahi --with-ssl=openssl --with-metadata --with-soxr --with-systemd
+sleep 1
+tst make
+sleep 1
+tst getent group shairport-sync &>/dev/null || sudo groupadd -r shairport-sync >/dev/null
+tst getent passwd shairport-sync &> /dev/null || sudo useradd -r -M -g shairport-sync -s /usr/bin/nologin -G audio shairport-sync >/dev/null
+sleep 1
 tst sudo make install
+sleep 1
 tst sudo systemctl enable shairport-sync
 
 echo "Done! You should reboot now"
