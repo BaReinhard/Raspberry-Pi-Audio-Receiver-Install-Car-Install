b4738f1e3ff081eb405ac3b2c399b6ddb1853382
Brett Reinhard <brett@reinhards.us>
2017-02-16 10:33:20 -0800

    Update runall.sh

--- runall.sh
+++ runall.sh
@@ -4,6 +4,7 @@ log="./runall.log"
 echo "" > $log
 
 read -p "Device name: " MYNAME
+read -p "Device WiFi Password: " WIFIPASS
 
 #--------------------------------------------------------------------
 function tst {
@@ -31,4 +32,23 @@ echo "--------------------------------------------" | tee -a $log
 tst ./ap_install.sh | tee -a $log
 echo "--------------------------------------------" | tee -a $log
 echo "${MYNAME}" | tst ./ap_config.sh | tee -a $log
+# Setup AP
+cat <<EOT >/etc/hostapd/hostapd.conf
+interface=wlan0
+driver=nl80211
+ssid=$MYNAME
+hw_mode=g
+channel=6
+macaddr_acl=0
+auth_algs=1
+ignore_broadcast_ssid=0
+wpa=2
+wpa_passphrase=$WIFIPASS
+wpa_key_mgmt=WPA-PSK
+#wpa_pairwise=TKIP      # You better do not use this weak encryption (only used by old client devices
+rsn_pairwise=CCMP
+ieee80211n=1          # 802.11n support
+wmm_enabled=1         # QoS support
+ht_capab=[HT40][SHORT-GI-20][DSSS_CCK-40]
+EOT
 echo "Ending at @ `date`" | tee -a $log
