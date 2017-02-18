9c0fea48a2a2aea39c37f49219395c3c430fd468
Brett Reinhard <brett@reinhards.us>
2017-02-17 17:58:30 -0800

    Update kodi_install.sh

--- kodi_install.sh
+++ kodi_install.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+#--------------------------------------------------------------------
+function tst {
+    echo "===> Executing: $*"
+    if ! $*; then
+        echo "Exiting script due to error from: $*"
+        exit 1
+    fi	
+}
+#--------------------------------------------------------------------
+
+
+# Install Dependencies
+
+
+
+exit 0
