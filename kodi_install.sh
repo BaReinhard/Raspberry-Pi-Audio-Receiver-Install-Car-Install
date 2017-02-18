d992d8dbd29ffdb60e23a3f489428e81d7d32dee
Brett Reinhard <brett@reinhards.us>
2017-02-18 12:52:46 -0800

    Update kodi_install.sh

--- kodi_install.sh
+++ kodi_install.sh
@@ -24,18 +24,7 @@ patch /etc/default/kodi << EOT
 
 EOT
 
-cat << EOT > /etc/udev/rules.d/10-permissions.rules
-# input
-KERNEL=="mouse*|mice|event*",   MODE="0660", GROUP="input"
-KERNEL=="ts[0-9]*|uinput",      MODE="0660", GROUP="input"
-KERNEL=="js[0-9]*",             MODE="0660", GROUP="input"
 
-# tty
-KERNEL=="tty[0-9]*",            MODE="0666"
-
-# vchiq
-SUBSYSTEM=="vchiq",  GROUP="video", MODE="0660"
-EOT
 usermod -a -G input kodi
 
 
